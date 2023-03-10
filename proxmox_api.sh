3cd0ea3f8e553a5e9c9b1f34413e0d95a9ff3b81
Sebastian Hanz <SebastianHanz@gmx.de>
2023-03-10 19:09:20 +0100

    Initial commit with changes
    -Added functionality to use a credentials_file
    -Improved ticket&token handling by only calling once and save message to temp file
    -Added additional options for lxc and vm control
    -Added selection to control lxc or vm directly ->"./proxmox_api.sh start vm 101"
    -Added "custom" function to simply execute any API command available without typing whole url

--- proxmox_api.sh
+++ proxmox_api.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-PROXMOX_NODE_IP=put-here-ip
-PROXMOX_NODE_NAME=put-here-name
-PROXMOX_STORAGE="put-here-storage-name"
+PROXMOX_NODE_IP=your-proxmox-ip-here #without port
+PROXMOX_NODE_NAME=your-node-name-here
+PROXMOX_STORAGE=put-here-storage-name
 
-API_USER=put-here-username
-API_USER_PASSWORD=put-here-password
+# Comment out next two lines if you use credential file like below
+API_USER=api-user-here
+API_USER_PASSWORD=api-password-here 
 
-CRED="username=$API_USER@pve&password=$API_USER_PASSWORD"
+# Uncomment next three lines if you want to store your credentials in a file
+#CREDENTIAL_FILE="/root/.proxmoxcredentials"                                #Customize your path
+#API_USER=$(sed -n 's/^API_USER=//p' $CREDENTIAL_FILE)                      #Get API_USER from file
+#API_USER_PASSWORD=$(sed -n 's/^API_USER_PASSWORD=//p' $CREDENTIAL_FILE)    #Get API_USER_PASSWORD from file
 
-curl --silent --insecure --data $CRED https://$PROXMOX_NODE_IP:8006/api2/json/access/ticket | jq --raw-output '.data.ticket' | sed 's/^/PVEAuthCookie=/' > cookie
-curl --silent --insecure --data $CRED https://$PROXMOX_NODE_IP:8006/api2/json/access/ticket | jq --raw-output '.data.CSRFPreventionToken' | sed 's/^/CSRFPreventionToken:/' > token
+# Manage ticket & token handling
+curl --silent --insecure -k -d  "username=$API_USER@pve" --data-urlencode "password=$API_USER_PASSWORD" https://$PROXMOX_NODE_IP:8006/api2/json/access/ticket > message
+jq --raw-output '.data.ticket' message | sed 's/^/PVEAuthCookie=/' > cookie
+jq --raw-output '.data.CSRFPreventionToken' message | sed 's/^/CSRFPreventionToken:/' > token
 
-# container config
+# Standard container config
 
 CPU=1
 CPUUNITS=512
@@ -22,20 +28,45 @@ SWAP=0
 OS_TEMPLATE="IMAGES:vztmpl/centos-8-default_20191016_amd64.tar.xz"
 
 
-# script options
+# Available script functions
 
 case $1 in
 
-    start|stop) curl --silent --insecure  --cookie "$(<cookie)" --header "$(<token)" -X POST https://$PROXMOX_NODE_IP:8006/api2/json/nodes/$PROXMOX_NODE_NAME/lxc/$2/status/$1; echo "  done." ;;
+start | stop | reboot | resume | shutdown | suspend)
+    if [ $2 == lxc ]; then
+        curl --silent --insecure --cookie "$(<cookie)" --header "$(<token)" -X POST https://$PROXMOX_NODE_IP:8006/api2/json/nodes/$PROXMOX_NODE_NAME/lxc/$3/status/$1
+        echo "  done."
+    elif [[ $2 == vm || $2 == qemu ]]; then
+        curl --silent --insecure --cookie "$(<cookie)" --header "$(<token)" -X POST https://$PROXMOX_NODE_IP:8006/api2/json/nodes/$PROXMOX_NODE_NAME/qemu/$3/status/$1
+        echo "  done."
+    fi
+    ;;
 
-    create) curl --insecure  --cookie "$(<cookie)" --header "$(<token)" -X POST --data-urlencode net0="name=tnet$2,bridge=vmbr0" --data ostemplate=$OS_TEMPLATE --data storage=$PROXMOX_STORAGE --data vmid=$2 --data cores=$CPU --data cpuunits=$CPUUNITS --data memory=$MEMORY --data swap=$SWAP --data hostname=ctnode$2 https://$PROXMOX_NODE_IP:8006/api2/json/nodes/$PROXMOX_NODE_NAME/lxc; echo "  done."  ;;
+custom)
+    curl --silent --insecure --cookie "$(<cookie)" --header "$(<token)" -X POST https://$PROXMOX_NODE_IP:8006/api2/json/nodes/$2
+    echo "  done."
+    ;;
 
-    delete) curl --silent --insecure  --cookie "$(<cookie)" --header "$(<token)" -X DELETE https://$PROXMOX_NODE_IP:8006/api2/json/nodes/$PROXMOX_NODE_NAME/lxc/$2;echo "  done." ;;
+create)
+    curl --insecure --cookie "$(<cookie)" --header "$(<token)" -X POST --data-urlencode net0="name=tnet$2,bridge=vmbr0" --data ostemplate=$OS_TEMPLATE --data storage=$PROXMOX_STORAGE --data vmid=$2 --data cores=$CPU --data cpuunits=$CPUUNITS --data memory=$MEMORY --data swap=$SWAP --data hostname=ctnode$2 https://$PROXMOX_NODE_IP:8006/api2/json/nodes/$PROXMOX_NODE_NAME/lxc
+    echo "  done."
+    ;;
 
-    *) echo ""; echo " usage:  start|stop|create|delete <vmid> ";echo ""; ;;
+delete)
+    curl --silent --insecure --cookie "$(<cookie)" --header "$(<token)" -X DELETE https://$PROXMOX_NODE_IP:8006/api2/json/nodes/$PROXMOX_NODE_NAME/lxc/$2
+    echo "  done."
+    ;;
+
+*)
+    echo ""
+    echo " usage:  start|stop|reboot|resume|shutdown|suspend lxc|vm <vmid> #Simply start,stop,reboot.. your lxc or vm"
+    echo " usage:  custom node3/lxc/155/status/reboot #Adding custom commands without typing static URL parts (https://$PROXMOX_NODE_IP:8006/api2/json/nodes/)"
+    echo " usage:  create|delete <vmid> "
+    echo ""
+    ;;
 
 esac
 
-# remove cookie and token
-
-rm cookie token
+# Remove temporary files
+rm cookie message token
+rm cookie message token
